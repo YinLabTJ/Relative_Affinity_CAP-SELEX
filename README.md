cf211bbb27f43e0f42a4acde2f5ff851f393a8c5
YinLabTJ <xzy_aq@tongji.edu.com>
2024-11-28 11:08:46 +0800

    update

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ perl	v5.26.1 or higher version
 R	v4.1.2 or higher version, library ggplot2 required
 
 
-export PATH="/data/software/Relative_Affinity_CAP-SELEX:$PATH"  #add spacek40 path to environment
+export PATH="/data/software/Relative_Affinity_CAP-SELEX:$PATH"  #add spacek40 path to your environment
 
 To run this pipeline, please refer to run_relative_affinity_example.sh.
--- run_relative_affinity_example.sh
+++ run_relative_affinity_example.sh
@@ -2,7 +2,7 @@
 
 # An example of computing relative affinity for our published CAP-SELEX matrices
 # To run this pipeline, you need a batch file with TF_pairs information (File Batch_test is an example)and a folder (e.g. ./SELEX) that stores sequence file
-# The sequence files should contain DNA sequence only, not fastq format. And the name of the sequence files should be consistent with the batch file.
+# The sequence files should contain 40bp DNA sequence only, not fastq format. And the name of the sequence files should be consistent with the batch file.
 # Take "Batch_test.txt" and folder “SELEX" as an example
 
 #export PATH="/data/software/Relative_Affinity_CAP-SELEX:$PATH"  #spacek40 path
@@ -11,7 +11,7 @@
 perl script/step1_spacek40.pl Batch_test.txt SELEX
 
 #2. computing relative affinity
-#filter_list was created with MI pipeline using the same batch_file
+#filter_list was created with MI pipeline (https://github.com/YinLabTJ/MI_CAP-SELEX) using the same batch_file
 for i in `grep -v HGNC Batch_test.txt | awk '{print $1"_"$3}'`
 do
 	perl script/step2.relative_affinity.pl Batch_test.txt $i 10 SELEX MI_output/filter_list
